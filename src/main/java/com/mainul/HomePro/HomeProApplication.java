d08b99738cae1208c6a182ec2d20f2b7c9207c58
maainul <mainul080@yahoo.com>
2021-03-23 11:19:41 +0600

    First Commit

---
+++ src/main/java/com/mainul/HomePro/HomeProApplication.java
@@ -0,0 +1,13 @@
+package com.mainul.HomePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeProApplication.class, args);
+	}
+
+}
