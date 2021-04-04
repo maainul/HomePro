50d6a8edb6debf98a9ea37a582332a52efffa0ff
maainul <mainul080@yahoo.com>
2021-04-05 01:42:12 +0600

    this month rent pdf

--- src/main/java/com/mainul/HomePro/controllers/RentController.java
+++ src/main/java/com/mainul/HomePro/controllers/RentController.java
@@ -1,10 +1,14 @@
 package com.mainul.HomePro.controllers;
 
+import com.lowagie.text.DocumentException;
+import com.mainul.HomePro.models.Expense;
 import com.mainul.HomePro.models.Rent;
 import com.mainul.HomePro.models.Room;
 import com.mainul.HomePro.service.FloorService;
 import com.mainul.HomePro.service.RentService;
 import com.mainul.HomePro.service.RoomService;
+import com.mainul.HomePro.utils.ExpensePDFExporter;
+import com.mainul.HomePro.utils.RentMonthWisePDFExporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,51 +17,79 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 @Controller
 public class RentController {
 
-    @Autowired private RoomService roomService;
-    @Autowired private RentService rentService;
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private RentService rentService;
 
     @GetMapping("/addRent")
-    public String getRentForm(Model model){
-        model.addAttribute("rooms",roomService.roomList());
+    public String getRentForm(Model model) {
+        model.addAttribute("rooms", roomService.roomList());
         model.addAttribute("rent", new Rent());
         return "addRent";
     }
 
     @PostMapping("/addRent")
-    public String saveRent(@ModelAttribute Rent rent){
+    public String saveRent(@ModelAttribute Rent rent) {
         rentService.saveRent(rent);
         return "redirect:/rentList";
     }
 
     @GetMapping("/rentList")
-    public String rentListTable(Model model){
-        model.addAttribute("roomList",roomService.roomList());
-        model.addAttribute("rentList",rentService.getAllRent());
+    public String rentListTable(Model model) {
+        model.addAttribute("roomList", roomService.roomList());
+        model.addAttribute("rentList", rentService.getAllRent());
         return "rentList";
     }
 
 
     @GetMapping("/updateRentInfo/{id}")
-    public String updateRentForm(@PathVariable(value = "id") Long id, Model model){
+    public String updateRentForm(@PathVariable(value = "id") Long id, Model model) {
         model.addAttribute("roomList", roomService.roomList());
-        model.addAttribute("rent",rentService.getRentById(id));
+        model.addAttribute("rent", rentService.getRentById(id));
         return "updateRentInfo";
     }
 
 
     @GetMapping("/thisMonthRentDetails")
-    public String thisMonthRentInfo(Model model){
-        model.addAttribute("thisMonthRentDetails",rentService.currentMonthRentList());
+    public String thisMonthRentInfo(Model model) {
+        model.addAttribute("thisMonthRentDetails", rentService.currentMonthRentList());
         return "thisMonthRentDetails";
     }
 
     @GetMapping("/thisYearRentList")
-    public String thisYearRentDetails(Model model){
-        model.addAttribute("thisYearRentDetails",rentService.currentYearRentList());
+    public String thisYearRentDetails(Model model) {
+        model.addAttribute("thisYearRentDetails", rentService.currentYearRentList());
         return "thisYearRentList";
+    }
+
+
+    @GetMapping("/rent/export/pdf")
+    public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException, ParseException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=expenses_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<Rent> listRents = rentService.currentMonthRentList();
+
+        RentMonthWisePDFExporter exporter = new RentMonthWisePDFExporter(listRents);
+        exporter.export(response);
+
     }
 
 }
---
+++ src/main/java/com/mainul/HomePro/utils/RentMonthWisePDFExporter.java
@@ -0,0 +1,95 @@
+package com.mainul.HomePro.utils;
+
+import com.lowagie.text.*;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.mainul.HomePro.models.Expense;
+import com.mainul.HomePro.models.Rent;
+
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class RentMonthWisePDFExporter {
+    private List<Rent> listRents;
+
+    public RentMonthWisePDFExporter(List<Rent> listRents) {
+        this.listRents = listRents;
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.BLUE);
+        cell.setPadding(5);
+
+        com.lowagie.text.Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.WHITE);
+
+        cell.setPhrase(new Phrase("Date", font));
+
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Room Name", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Room Rent", font));
+        table.addCell(cell);
+
+        cell.setPhrase(new Phrase("Gas Bill", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Internet Bill", font));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Electricity Bill", font));
+        table.addCell(cell);
+
+    }
+
+    private void writeTableData(PdfPTable table) throws ParseException, FileNotFoundException {
+        for (Rent rent : listRents) {
+            Date date = rent.getRentalPaymentDate();
+            Format formatter = new SimpleDateFormat("dd/MM/yyyy");
+            table.addCell(formatter.format(date));
+            table.addCell(rent.getRoom().getRoomName());
+            table.addCell(String.valueOf(rent.getRoomRent()));
+            table.addCell(String.valueOf(rent.getGasBill()));
+            table.addCell(String.valueOf(rent.getInternetBill()));
+            table.addCell(String.valueOf(rent.getElectricityBill()));
+
+        }
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException, ParseException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(18);
+        font.setColor(Color.BLUE);
+
+        Paragraph p = new Paragraph("List of Rents", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        PdfPTable table = new PdfPTable(6);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {1.5f, 3.5f, 3.0f, 3.0f, 3.0f,2.0f});
+        table.setSpacingBefore(10);
+
+        writeTableHeader(table);
+        writeTableData(table);
+
+        document.add(table);
+
+        document.close();
+    }
+}
