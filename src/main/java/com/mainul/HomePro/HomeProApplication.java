5b5bfc8595ef156dee7a3ef02573555149dec3dc
maainul <mainul080@yahoo.com>
2021-04-10 15:29:23 +0600

    update

--- src/main/java/com/mainul/HomePro/HomeProApplication.java
+++ src/main/java/com/mainul/HomePro/HomeProApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class HomeProApplication {
 
 	public static void main(String[] args) {
